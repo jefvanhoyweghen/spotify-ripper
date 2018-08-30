f797317ba5f0c7af386c949b8d12f5fd3ac84b77
Jef Van Hoyweghen <jef@socialseeder.com>
2018-08-30 14:19:13 +0200

    setup

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def _read(fn):
 
 setup(
     name='spotify-ripper',
-    version='2.10.5',
+    version='2.11',
     packages=find_packages(exclude=["tests"]),
     scripts=['spotify_ripper/main.py'],
     include_package_data=True,
@@ -55,8 +55,8 @@ setup(
                 'to audio files',
     license='MIT',
     keywords="spotify ripper mp3 ogg vorbis flac opus acc mp4 m4a",
-    url='https://github.com/jrnewell/spotify-ripper',
-    download_url='https://github.com/jrnewell/spotify-ripper/tarball/2.10.5',
+    url='https://github.com/jefvanhoyweghen/spotify-ripper',
+    download_url='https://github.com/jefvanhoyweghen/spotify-ripper/tarball/2.11',
     classifiers=[
         'Topic :: Multimedia :: Sound/Audio',
         'Topic :: Multimedia :: Sound/Audio :: Capture/Recording',
